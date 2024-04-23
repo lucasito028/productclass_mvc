8245273a151a18e987c26c1b810a35cc1f850a52
lucasito028 <lucastoshihiroito28@gmail.com>
2024-04-23 14:14:00 -0300

    Melhorias

--- ProductController.js
+++ ProductController.js
@@ -2,75 +2,134 @@ import {Product} from './Product.js';
 
 export class ProductController {
 
-    constructor() {
-        productsPossible = [
+    productsPossible = [
+        new Product("Produto 1", 10.10, 20),
+        new Product("Produto 2", 20.30, 50),
+        new Product("Produto 3", 15.50, 36),
+        new Product("Produto 4", 10.90, 45),
+        new Product("Produto 5", 10.20, 35),
+        new Product("Produto 6", 10.10, 45),
+        new Product("Produto 7", 12.44, 27),
+        new Product("Produto 8", 17.90, 28),
+        new Product("Produto 9", 10.80, 39),
+    ];
+    productsInTheCart = [
+        new Product("Produto 1", 10.10, 2),
+    ];
+    msg="";
 
-            new Product("Produto 1", 10.10, 20),
-            new Product("Produto 2", 20.30, 50),
-            new Product("Produto 3", 15.50, 36),
-            new Product("Produto 4", 10.90, 45),
-            new Product("Produto 5", 10.20, 35),
-            new Product("Produto 6", 10.10, 45),
-            new Product("Produto 7", 12.44, 27),
-            new Product("Produto 8", 17.90, 28),
-            new Product("Produto 9", 10.80, 39),
-            
-        ];
-
-        productsInTheCart = [
-            new Product("Produto 1", 10.10, 2),
-        ];
-    }
-
+    //Basic Function
     output(message){
-        alert (message);
+        alert(message);
     }
 
-    listallProductfromcart() {
-        msg = `Lista de Todos os Produtos`;
-
-    }
-
-    // Calculate Brute value
-    calculateTotal() {
-       
-    }
-
-
-    
-    /* Controllers Costumers Functions */
+    /* Costumers Functions */
     addToCart(id, qtd) {
+        const productToAdd = this.productsPossible[id - 1]; 
 
+        if (!productToAdd) {
+            this.output("Produto não encontrado.");
+            return;
+        }
+        if (productToAdd.stock < qtd) {
+            this.output("Quantidade solicitada indisponível em estoque.");
+            return;
+        }
+    
+        const existingProductIndex = this.productsInTheCart.findIndex(product => product.name === productToAdd.name);
+    
+        if (existingProductIndex !== -1) {
+            this.productsInTheCart[existingProductIndex].quantity += qtd;
+        } else {
+            const productCopy = new Product(productToAdd.name, productToAdd.price, qtd);
+            this.productsInTheCart.push(productCopy);
+        }
+    
+        productToAdd.stock -= qtd;
+    
+        this.output(`${qtd} unidades de ${productToAdd.name} adicionadas ao carrinho.`);
     }
+    listallProductFromCart() {
+        if (this.productsInTheCart.length === 0) {
+            this.output("O carrinho está vazio.");
+            return;
+        }
+    
+        this.msg = `Lista de Todos os Produtos`;
+        this.productsInTheCart.forEach((product, index) => {
+            this.msg += `${index + 1}. Produto: ${product.name}, Preço Unitário: ${product.price.toFixed(2)}, Quantidade: ${product.quantity}\n`;
+        });
 
-
-    // Altera a quantidade de um produto do carrinho pelo índice do array
-    alterstockfromcart(id, qtd) {
+        this.output(this.msg);
     }
-
-    removeproductFromCart(id) {
-        
+    alterStockFromCart(id, qtd) {
+        if (id < 1 || id > this.productsInTheCart.length) {
+            this.output("ID de produto inválido.");
+            return;
+        }
+    
+        const productToUpdate = this.productsInTheCart[id - 1];
+    
+        if (!productToUpdate) {
+            this.output("Produto não encontrado no carrinho.");
+            return;
+        }
+    
+        const productPossible = this.productsPossible.find
+        (product => product.name === productToUpdate.name);
+        const currentStock = productPossible ? productPossible.stock + productToUpdate.quantity : 0;
+    
+        if (currentStock < qtd) {
+            this.output("Quantidade solicitada indisponível em estoque.");
+            return;
+        }
+    
+        productPossible.stock -= (qtd - productToUpdate.quantity);
+        productToUpdate.quantity = qtd;
+    
+        this.output(`Quantidade do produto ${productToUpdate.name} atualizada para ${qtd}.`);
     }
+    removeProductFromCart(id) {
+        if (id < 1 || id > this.productsInTheCart.length) {
+            this.output("ID de produto inválido.");
+            return;
+        }
+    
+        const removedProduct = this.productsInTheCart.splice(id - 1, 1)[0]; 
 
-    calculateSubtotalByIndex(id) {
+        const productPossible = this.productsPossible.find(product => product.name === removedProduct.name);
+        if (productPossible) {
+            productPossible.stock += removedProduct.quantity;
+        }
+    
+        this.output(`O produto ${removedProduct.name} foi removido do carrinho.`);
     }
-
+    calculateSubtotalById(id) {
+        if (id < 1 || id > this.productsInTheCart.length) {
+            this.output("ID de produto inválido.");
+            return;
+        }
+    
+        const product = this.productsInTheCart[id - 1];
+        const subtotal = product.price * product.quantity;
+    
+        this.output(`Subtotal do produto ${product.name}: $${subtotal.toFixed(2)}`);
+    }
     // Just implments discount
-    implementsdiscount() {
+    implementsDiscount() {
     }
+    calculateTotal(){
 
+    }
     checkout() {
         alert("Venda finalizada. Obrigado por comprar conosco!");
-        this.productsInCart = []; 
+        this.productsInTheCart = []; 
     }
 
 
 
-    
-    /* Controllers Admin Functions*/
-
+    /*Admin Functions*/
     createProduct(name, price, stock) {
-
         const newProduct = new Product(name, price, stock);
 
         this.productsPossible.push(newProduct);
@@ -79,22 +138,22 @@ export class ProductController {
     }
     
     updateProduct(id, new_name, new_price, new_stock) {
+        const productToUpdate = this.productsPossible[id];
 
-        const product_to_update = this.products_possible[id];
-
-        if (!product_to_update) {
+        if (!productToUpdate) {
             this.output("Produto não encontrado.");
             return;
         }
-        product_to_update.name = new_name;
-        product_to_update.price = new_price;
-        product_to_update.stock = new_stock;
+        productToUpdate.name = new_name;
+        productToUpdate.price = new_price;
+        productToUpdate.stock = new_stock;
 
         this.output(`${newname} atualizado.`);
     }
     
     readProduct(id) {
-        const product = this.products_possible[id];
+        const product = this.productsPossible[id];
+
         if (!product) {
             this.output("Produto não encontrado.");
             return;
@@ -103,24 +162,25 @@ export class ProductController {
     }
     
     listallProduct() {
-        let msg;
-        msg += "Lista de todos os produtos:\n\n";
-        this.products_possible.forEach((product, index) => {
-            msg += `${index + 1}. Nome: ${product.name}, Preço: ${product.price}, Estoque: ${product.stock}\n`;
+        this.msg += "Lista de todos os produtos:\n\n";
+        this.productsPossible.forEach((product, id) => {
+            this.msg += `${id + 1}. Nome: ${product.name}, Preço: ${product.price}, Estoque: ${product.stock}\n`;
         });
-        this.output(msg);
+        this.output(this.msg);
     }
     
     deleteProduct(id) {
         const deletedProduct = this.productsPossible.splice(id, 1);
+
         if (deletedProduct.length === 0) {
             this.output("Produto não encontrado.");
             return;
         }
+
         this.output(`${deletedProduct[0].name} removido da lista de produtos.`);
     }
     
-    updatestockProduct(id, new_stock, time) {
+    updateStockProduct(id, new_stock, time) {
         const product = this.productsPossible[id];
 
         if (!product) {
@@ -132,7 +192,7 @@ export class ProductController {
             .catch(error => this.output(error));
     }
     
-    createciscount(id, discount) {
+    createDiscount(id, discount) {
         const product = this.productsPossible[id];
 
         if (!product) {
--- ProductView.js
+++ ProductView.js
@@ -5,111 +5,130 @@ class ProductView{
     constructor(){
         let ProductController = new ProductController();
     }
+
     static main(){
-
-        /*
-        // Criar uma instância de Product
-        const product = new Product("Produto 1", 10, 100);
-        
-        // Testar o método showProduct
-        console.log("Detalhes do produto:");
-        console.log(product.showProduct());
-
-        // Testar o método calcularSubtotal
-        const subtotal = product.calcularSubtotal(5);
-        console.log("\nSubtotal do produto para 5 unidades:");
-        console.log(subtotal);
-
-        // Testar o método updateStock (usando Promises)
-        console.log("\nAtualizando o estoque do produto:");
-        product.updateStock(3, 50)
-            .then(response => console.log(response))
-            .catch(error => console.error(error));
-
-        // Testar o método createDiscount (usando Promises)
-        console.log("\nCriando desconto para o produto:");
-        product.createDiscount(2, 10)
-            .then(response => console.log(response))
-            .catch(error => console.error(error));
-    */
-    let escolha;
+        let escolha;
+                    
+        while (true) {  
+        escolha =
+        prompt(`Escolha uma opção:\n
+        1 - No Cliente\n
+        2 - No ADMIN\n
+        3 - Sair`);
                 
-    while (true) {
-                  // Solicita ao usuário que escolha uma opção
-    escolha = prompt(`Escolha uma opção:\n
-    1 - No Cliente\n
-    2 - No ADMIN\n
-    3 - Sair`);
-              
-    escolha = parseInt(escolha);
-              
-            switch (escolha) {
-                case 1:
-                    alert("Você selecionou a opção No Cliente.");
-                    ProductView.costumerchoise();
+        escolha = parseInt(escolha);
+                
+        switch (escolha) {
+            case 1:
+                alert("Você selecionou a opção No Cliente.");
+                ProductView.costumerChoise();
+            break;
+            case 2:
+                alert("Você selecionou a opção No ADMIN.");
+                ProductView.adminChoise();
                 break;
-                case 2:
-                    alert("Você selecionou a opção No ADMIN.");
-                    //this.adminchoise();
+            case 3:
+                alert("Você selecionou a opção Sair. Encerrando o ciclo.");
+                return; 
+            default:
+                alert("Opção inválida. Tente novamente.");
                 break;
-                case 3:
-                    alert("Você selecionou a opção Sair. Encerrando o ciclo.");
-                    return; 
-                default:
-                    alert("Opção inválida. Tente novamente.");
-                break;
-                  }
                 }
-            }
-            
-            static costumerchoise() {
-                while (true) {
-                    let c1 = prompt(`Escolha uma opção como cliente:\n
-                    1 - Adicionar Produto no Carrinho\n
-                    2 - Remover um Item do Carrinho\n
-                    3 - Alterar Quantidade de um Produto no Carrinho\n
-                    4 - Ver Subtotal de um Produto no Carrinho\n
-                    5 - Finalizar Compra\n
-                    0 - Sair do Cliente`);
-            
-                    c1 = parseInt(c1);
-            
-                    switch (c1) {
-                        case 1:
-                            let productIndexToAdd = parseInt(prompt("Digite o índice do produto que deseja adicionar:"));
-                            let quantityToAdd = parseInt(prompt("Digite a quantidade que deseja adicionar:"));
-                            this.ProductController.addToCart(productIndexToAdd, quantityToAdd);
-                            break;
-                        case 2:
-                            let productIndexToRemove = prompt("Digite o índice do produto que deseja remover:");
-                            let quantityToRemove = prompt("Digite a quantidade que deseja remover:");
-                            this.ProductController.removestockFromCart(productIndexToRemove, quantityToRemove);
-                            break;
-                        case 3:
-                            let productIndexToChange = prompt("Digite o índice do produto que deseja alterar:");
-                            let quantityToChange = prompt("Digite a quantidade desejada:");
-                            this.ProductController.alterstockfromcart(productIndexToChange, quantityToChange);
-                            break;
-                        case 4:
-                            let productIndexToCalculate = prompt("Digite o índice do produto que deseja calcular o subtotal:");
-                            this.ProductController.calculateSubtotal(productIndexToCalculate);
-                            break;
-                        case 5:
-                            this.ProductController.checkout();
-                            break;
-                        case 0:
-                            alert("Você saiu do Cliente.");
-                            return;
-                        default:
-                            alert("Opção inválida. Tente novamente.");
-                            break;
+        }
+    }
+                
+    static costumerChoise() {
+                    while (true) {
+                        let c1 = 
+                        prompt(`Escolha uma opção como cliente:\n
+                        1 - Adicionar Produto no Carrinho\n
+                        2 - Remover um Item do Carrinho\n
+                        3 - Alterar Quantidade de um Produto no Carrinho\n
+                        4 - Ver Subtotal de um Produto no Carrinho\n
+                        5 - Finalizar Compra\n
+                        0 - Sair do Cliente`);
+                
+                        c1 = parseInt(c1);
+                
+                        switch (c1) {
+                            case 1:
+                                let productIndexToAdd = parseInt(prompt("Digite o índice do produto que deseja adicionar:"));
+                                let quantityToAdd = parseInt(prompt("Digite a quantidade que deseja adicionar:"));
+                                this.ProductController.addToCart(productIndexToAdd, quantityToAdd);
+                                break;
+                            case 2:
+                                let productIndexToRemove = prompt("Digite o índice do produto que deseja remover:");
+                                let quantityToRemove = prompt("Digite a quantidade que deseja remover:");
+                                this.ProductController.removestockFromCart(productIndexToRemove, quantityToRemove);
+                                break;
+                            case 3:
+                                let productIndexToChange = prompt("Digite o índice do produto que deseja alterar:");
+                                let quantityToChange = prompt("Digite a quantidade desejada:");
+                                this.ProductController.alterstockfromcart(productIndexToChange, quantityToChange);
+                                break;
+                            case 4:
+                                let productIndexToCalculate = prompt("Digite o índice do produto que deseja calcular o subtotal:");
+                                this.ProductController.calculateSubtotal(productIndexToCalculate);
+                                break;
+                            case 5:
+                                this.ProductController.checkout();
+                                break;
+                            case 0:
+                                alert("Você saiu do Cliente.");
+                                return;
+                            default:
+                                alert("Opção inválida. Tente novamente.");
+                                break;
+                        }
                     }
-                }
+            }     
+
+    static adminChoise() {
+                    while (true) {
+                        let c1 = 
+                        prompt(`Escolha uma opção como Admin:\n
+                        1 - Cadastrar um Produto\n
+                        2 - Update o Produto\n
+                        3 - Alterar Produto\n
+                        4 - Ver Subtotal de um Produto no Carrinho\n
+                        5 - Implementar desconto\n
+                        0 - Sair como Admin`);
+                
+                        c1 = parseInt(c1);
+                
+                        switch (c1) {
+                            case 1:
+                                let productIndexToAdd = parseInt(prompt("Digite o índice do produto que deseja adicionar:"));
+                                let quantityToAdd = parseInt(prompt("Digite a quantidade que deseja adicionar:"));
+                                this.ProductController.addToCart(productIndexToAdd, quantityToAdd);
+                                break;
+                            case 2:
+                                let productIndexToRemove = prompt("Digite o índice do produto que deseja remover:");
+                                let quantityToRemove = prompt("Digite a quantidade que deseja remover:");
+                                this.ProductController.removestockFromCart(productIndexToRemove, quantityToRemove);
+                                break;
+                            case 3:
+                                let productIndexToChange = prompt("Digite o índice do produto que deseja alterar:");
+                                let quantityToChange = prompt("Digite a quantidade desejada:");
+                                this.ProductController.alterstockfromcart(productIndexToChange, quantityToChange);
+                                break;
+                            case 4:
+                                let productIndexToCalculate = prompt("Digite o índice do produto que deseja calcular o subtotal:");
+                                this.ProductController.calculateSubtotal(productIndexToCalculate);
+                                break;
+                            case 5:
+                                this.ProductController.checkout();
+                                break;
+                            case 0:
+                                alert("Você como Admin");
+                                return;
+                            default:
+                                alert("Opção inválida. Tente novamente.");
+                                break;
+                        }
+                    }
             }
 
-    /*adminchoise(){
-        
-    }*/
    }
 
 ProductView.main();
